7cdb0f639f7e7d1bb8d65e15b59f5e96b10bc5ae
thinius <thinius@users.noreply.github.com>
2015-04-01 16:08:22 +0200

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+package business-calendar;
+
+public class HelloWorld {
+
+	public void run() {
+	  System.out.println("Hello World!");
+	}
+	
+	public static void main(String[] args) {
+		HelloWorld helloWorld = new HelloWorld();
+		helloWorld.run();
+	}
+	
+}
